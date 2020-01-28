aa512f7da9bf7d6958bf0bff9a54f91699fb6d1f
cpon00 <cpon712@gmail.com>
2020-01-28 12:26:59 -0800

    added delete functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import TodoList from './TodoList'
 
-function App() {
+export default function App() {
+  const[todos, setTodos] = useState([
+    {id: 1, content: 'Explain to Masao what Smite does'},
+    {id: 1, content: 'Explore Unity Tutorials'},
+  ]);
+  function deleteTodo(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
+
   return (
     <div className="App">
       <h1>Things To Do!</h1>
+      <TodoList todos = {todos} />
     </div>
   );
 }
-
-export default App;
