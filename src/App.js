c95279f54b2a416fb4cf5f3f9fbf1e0f37e21fe5
cpon00 <cpon712@gmail.com>
2020-01-28 13:15:04 -0800

    added styling, finished tutorial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,28 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
+import Paper from '@material-ui/core/Paper'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 import TodoList from './TodoList'
+import TodoEntryForm from './TodoEntryForm';
 
 export default function App() {
-  const[todos, setTodos] = useState([
-    {id: 1, content: 'Explain to Masao what Smite does'},
-    {id: 1, content: 'Explore Unity Tutorials'},
+  const [todos, setTodos] = useState([
+    {id: 1, content: 'Teach Masao what Smite Does'},
+    {id: 2, content: 'Explore Unity Tutorials'},
   ]);
   function deleteTodo(id) {
     setTodos(todos => todos.filter(todo => todo.id !== id));
   }
-
+  function addTodo(todo) {
+    setTodos(todos => [...todos, todo]);
+  }
   return (
-    <div className="App">
-      <h1>Things To Do!</h1>
-      <TodoList todos = {todos} />
-    </div>
+    <Container maxWidth="sm" style={{marginTop: 20}}>
+      <Paper style={{padding: 20, backgroundColor: '#fffff3'}}>
+        <Typography variant="h3" align="center">Things To Do</Typography>
+        <TodoList todos={todos} deleter={deleteTodo}/>
+        <TodoEntryForm adder={addTodo} />
+      </Paper>
+    </Container>
   );
 }
